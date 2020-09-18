b759c26b4fcac9cc5564752899674ebec770a30a
Bruno Flores <bruno@brunoflores.com.br>
2020-09-18 14:30:44 +1000

    Copy files from `STM32Cube_FW_F3_V1.11.1`. `/STM32Cube_FW_F3_V1.11.1/Projects/STM32F3-Discovery/Examples/PWR/PWR_CurrentConsumption`

---
+++ build/startup_stm32f303xc.d
@@ -0,0 +1,1 @@
+build/startup_stm32f303xc.o: SW4STM32/startup_stm32f303xc.s
---
+++ build/stm32f3xx_lp_modes.d
@@ -0,0 +1,80 @@
+build/stm32f3xx_lp_modes.o: Src/stm32f3xx_lp_modes.c Inc/main.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ Inc/stm32f3xx_hal_conf.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Include/core_cm4.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Include/cmsis_version.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Include/cmsis_compiler.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Include/cmsis_gcc.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Include/mpu_armv7.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_exti.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rtc.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rtc_ex.h \
+ STM32Cube_FW_F3_V1.11.1/Drivers/BSP/STM32F3-Discovery/stm32f3_discovery.h \
+ Inc/stm32f3xx_lp_modes.h Inc/main.h
+
+Inc/main.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+
+Inc/stm32f3xx_hal_conf.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Include/core_cm4.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Include/cmsis_version.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Include/mpu_armv7.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_exti.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rtc.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rtc_ex.h:
+
+STM32Cube_FW_F3_V1.11.1/Drivers/BSP/STM32F3-Discovery/stm32f3_discovery.h:
+
+Inc/stm32f3xx_lp_modes.h:
+
+Inc/main.h:
